2eb4562edad6eee1b5df0b8cd45f5291b557d560
Sirens007_host <2921646312@qq.com>
2026-02-04 18:39:30 +0800

    修改域名配置

--- src/config.ts
+++ src/config.ts
@@ -24,7 +24,7 @@ const SITE_TIMEZONE = 8; //设置你的网站时区 from -12 to 12 default in UT
 export const siteConfig: SiteConfig = {
 	title: "Sirens",
 	subtitle: "Blog",
-	siteURL: "http://sirens007.cn/", // 请替换为你的站点URL，以斜杠结尾
+	siteURL: "http://blog.sirens007.cn/", // 请替换为你的站点URL，以斜杠结尾
 	siteStartDate: "2025-09-30", // 站点开始运行日期，用于站点统计组件计算运行天数
 
 	timeZone: SITE_TIMEZONE,
