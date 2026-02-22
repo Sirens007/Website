51328e7188447053e7b0d849ceb186a48a82c402
Sirens007_host <2921646312@qq.com>
2026-02-23 02:48:51 +0800

    删除备案信息使手机端背景完整

--- src/config.ts
+++ src/config.ts
@@ -492,18 +492,6 @@ export const footerConfig: FooterConfig = {
        style="color:inherit;text-decoration:none;">
       桂ICP备2026000608号
     </a>
-
-    <span style="color:inherit;">|</span>
-
-    <a href="http://www.beian.gov.cn/portal/registerSystemInfo?recordcode=45031202000488"
-       target="_blank"
-       rel="noopener"
-       style="color:inherit;text-decoration:none;display:inline-flex;align-items:center;">
-      <img src="/beian.png"
-           alt="公安备案图标"
-           style="height:14px;margin-right:4px;vertical-align:middle;">
-      桂公网安备 45031202000488号
-    </a>
   </div>
 </footer>
   `.trim(), // HTML格式的自定义页脚信息，例如备案号等，默认留空
