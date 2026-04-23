e5e6a26c2d13d2c3f6f7d4c94dfd3f5b79d174ae
Sirens007_host <2921646312@qq.com>
2026-04-23 22:37:24 +0800

    修改前端版本，twikoo本地化搭建

--- src/config.ts
+++ src/config.ts
@@ -447,7 +447,7 @@ export const commentConfig: CommentConfig = {
 	enable: true, // 启用评论功能。当设置为 false 时，评论组件将不会显示在文章区域。
 	system: "twikoo", // 评论系统选择: "twikoo" | "giscus"
 	twikoo: {
-		envId: "https://twikoo.sirens007.cn/.netlify/functions/twikoo",
+		envId: "https://twikoo.sirens007.cn", //https://twikoo.sirens007.cn/.netlify/functions/twikoo
 		lang: SITE_LANG,
 	},
 	giscus: {
