01ce4338dcd25749e4fd14e4db9bbdd8bd042bc1
Sirens007_host <2921646312@qq.com>
2026-05-01 00:15:19 +0800

    initialize configuration

--- src/env.d.ts
+++ src/env.d.ts
@@ -1,12 +1,2 @@
 /// <reference types="astro/client" />
 /// <reference path="../.astro/types.d.ts" />
-
-interface ImportMetaEnv {
-	readonly PUBLIC_UMAMI_WEBSITE_ID?: string;
-	readonly PUBLIC_UMAMI_API_ENDPOINT?: string;
-	readonly PUBLIC_UMAMI_BASE_URL?: string;
-	readonly PUBLIC_UMAMI_SHARE_URL?: string;
-	readonly PUBLIC_UMAMI_API_KEY?: string;
-	readonly PUBLIC_UMAMI_TOKEN?: string;
-	readonly PUBLIC_GTM_ID?: string;
-}
